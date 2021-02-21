f83e4360d3e9f6e85c82d28ee04b7dd5c569ee92
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-21 11:02:09 -0600

    completed code

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import DataArea from "./components/DataArea"
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+       
+    
+     <DataArea />
+  
+
+    </div>
+  );
+}
+
+export default App;
